7c07c0abd024e80c449ad7f8800ff28f8bfde0d6
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2025-01-06 17:00:43 -0600

    Update aurebesh.py
    added controls, option to print via HID

--- aurebesh.py
+++ aurebesh.py
@@ -5,6 +5,7 @@ import neopixel
 import random
 import touchio
 from ncount import *
+from prt import *
 
 
 touch1 = touchio.TouchIn(board.TOUCH1)
@@ -14,10 +15,42 @@ from aure import *
 
 sayings = ["live long and prosper","may the force be with you","size matters not"]
 
+REPL = True #True for use in IDE, False for printing via HID. Will blink red until you touch 1 or 2
+
+
+compthink()
+
+if REPL == False:
+    Val=0
+    while Val==0:
+        blinknum(1,red)
+        if touch1.value:
+            Val = Val + 1
+        if touch2.value:
+            Val = Val + 2
+    
+
+#touch 1 for Aurebesh alphabet, 2 for random saying in Aurebesh.
+
 while True:
-    saying = random.choice(sayings)
-    print(saying)
-    blinknum(2,green)
-    doAure(saying)
-    compthink()
-    time.sleep(2)
+    Val = 0
+    
+    if touch1.value:
+        Val = Val + 1
+        
+    
+    if touch2.value:
+        Val = Val + 2
+    if Val == 2:
+        saying = random.choice(sayings)
+        prt(saying,REPL)
+        blinknum(2,green)
+        doAure(saying,.1,REPL)
+        compthink()
+    if Val == 1:
+        for i in range(26):
+            l=(chr(ord("a")+i))
+            prt(l,REPL)
+            doAure(l,.25,REPL)
+            
+    time.sleep(.1)
